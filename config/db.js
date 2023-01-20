d269ad4121afbfb824c581ef309ef5d0838a70e8
ashelake <ashelake88@gmail.com>
2023-01-20 13:25:46 +0530

    signup

---
+++ config/db.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+const connection = mongoose.connect(
+  "mongodb+srv://ashelake:325411@cluster0.zshh9qv.mongodb.net/?retryWrites=true&w=majority"
+);
+module.exports = { connection };
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const { connection } = require("./config/db");
+
+const { UserModel } = require("./Modal/User.Modal");
 
 const app = express();
 
@@ -7,7 +10,22 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+app.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
 
-app.listen(8080, () => {
-  console.log("Listening to port 8080");
+  const new_user = new UserModel({
+    email,
+    password,
+  });
+  await new_user.save();
+  res.send("Sign up Successfull");
+});
+
+app.listen(8080, async () => {
+  try {
+    await connection;
+    console.log("Listening to port 8080");
+  } catch (err) {
+    console.log(err);
+  }
 });
